a6ba83c1ae0b11bae8c87d62136ee6f27e6a519d
William Addesso <wsaddesso@gmail.com>
2021-06-17 16:44:35 -0400

    Changed console logged information in getData function.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ async function getData(city) {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=149ffce1bb4cd39b05ac9f60cbf67c14`
     );
-    const cityWeather = await response.json();
-    console.log(cityWeather.main);
+    const cityInfo = await response.json();
+    const cityWeather = cityInfo.main;
+    console.log(
+      `Current Temp: ${cityWeather.temp}, High: ${cityWeather.temp_max}, Low: ${cityWeather.temp_min}`
+    );
   } catch (error) {
     console.log(error, 'There was an error getting the weather data');
   }
 }
 
-getData('London');
+getData('Eastchester');
