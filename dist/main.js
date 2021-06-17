0eea35d9774116613ac9fc796527ed0a605f6c29
William Addesso <wsaddesso@gmail.com>
2021-06-17 16:20:57 -0400

    Added try/catch block with error handling.

--- dist/main.js
+++ dist/main.js
@@ -4,15 +4,19 @@ var __webpack_exports__ = {};
   !*** ./src/index.js ***!
   \**********************/
 async function getData(city) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=149ffce1bb4cd39b05ac9f60cbf67c14`
-  );
-  const cityWeather = await response.json();
-  console.log(cityWeather.main);
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=149ffce1bb4cd39b05ac9f60cbf67c14`
+    );
+    const cityWeather = await response.json();
+    console.log(cityWeather.main);
+  } catch (error) {
+    console.log(error, 'There was an error getting the weather data');
+  }
 }
 
 getData('London');
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyLWFwcC8uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSx5REFBeUQsS0FBSztBQUM5RDtBQUNBO0FBQ0E7QUFDQTs7QUFFQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiYXN5bmMgZnVuY3Rpb24gZ2V0RGF0YShjaXR5KSB7XG4gIGNvbnN0IHJlc3BvbnNlID0gYXdhaXQgZmV0Y2goXG4gICAgYGh0dHBzOi8vYXBpLm9wZW53ZWF0aGVybWFwLm9yZy9kYXRhLzIuNS93ZWF0aGVyP3E9JHtjaXR5fSZ1bml0cz1pbXBlcmlhbCZhcHBpZD0xNDlmZmNlMWJiNGNkMzliMDVhYzlmNjBjYmY2N2MxNGBcbiAgKTtcbiAgY29uc3QgY2l0eVdlYXRoZXIgPSBhd2FpdCByZXNwb25zZS5qc29uKCk7XG4gIGNvbnNvbGUubG9nKGNpdHlXZWF0aGVyLm1haW4pO1xufVxuXG5nZXREYXRhKCdMb25kb24nKTtcbiJdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyLWFwcC8uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBLDJEQUEyRCxLQUFLO0FBQ2hFO0FBQ0E7QUFDQTtBQUNBLEdBQUc7QUFDSDtBQUNBO0FBQ0E7O0FBRUEiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbImFzeW5jIGZ1bmN0aW9uIGdldERhdGEoY2l0eSkge1xuICB0cnkge1xuICAgIGNvbnN0IHJlc3BvbnNlID0gYXdhaXQgZmV0Y2goXG4gICAgICBgaHR0cHM6Ly9hcGkub3BlbndlYXRoZXJtYXAub3JnL2RhdGEvMi41L3dlYXRoZXI/cT0ke2NpdHl9JnVuaXRzPWltcGVyaWFsJmFwcGlkPTE0OWZmY2UxYmI0Y2QzOWIwNWFjOWY2MGNiZjY3YzE0YFxuICAgICk7XG4gICAgY29uc3QgY2l0eVdlYXRoZXIgPSBhd2FpdCByZXNwb25zZS5qc29uKCk7XG4gICAgY29uc29sZS5sb2coY2l0eVdlYXRoZXIubWFpbik7XG4gIH0gY2F0Y2ggKGVycm9yKSB7XG4gICAgY29uc29sZS5sb2coZXJyb3IsICdUaGVyZSB3YXMgYW4gZXJyb3IgZ2V0dGluZyB0aGUgd2VhdGhlciBkYXRhJyk7XG4gIH1cbn1cblxuZ2V0RGF0YSgnTG9uZG9uJyk7XG4iXSwic291cmNlUm9vdCI6IiJ9
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 async function getData(city) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=149ffce1bb4cd39b05ac9f60cbf67c14`
-  );
-  const cityWeather = await response.json();
-  console.log(cityWeather.main);
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=149ffce1bb4cd39b05ac9f60cbf67c14`
+    );
+    const cityWeather = await response.json();
+    console.log(cityWeather.main);
+  } catch (error) {
+    console.log(error, 'There was an error getting the weather data');
+  }
 }
 
 getData('London');
