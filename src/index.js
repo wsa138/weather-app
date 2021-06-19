ada146ade4873cb66349caec47efc8e25cb7b5e7
William Addesso <wsaddesso@gmail.com>
2021-06-19 12:52:36 -0400

    Refactored getWeather function and how it uses object destructuring to get values. Wrap both functions in a runApp async function to test functionality.

--- src/index.js
+++ src/index.js
@@ -6,17 +6,38 @@ async function getData(city) {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=149ffce1bb4cd39b05ac9f60cbf67c14`
     );
-    const responseConverted = await response.json();
-    return responseConverted;
+    const responseJson = await response.json();
+    return responseJson;
   } catch (error) {
     console.log(error, 'There was an error getting the weather info');
   }
 }
 
 // Return weather info as object.
-async function getWeather(obj) {
-  const data = await obj;
-  console.log(data.main.temp);
+async function getWeather(weatherObj) {
+  const {
+    main: { temp },
+    main: { temp_max },
+    main: { temp_min },
+    main: { humidity },
+    sys: { sunrise },
+    sys: { sunset },
+    timezone,
+    weather: {
+      [0]: { description },
+    },
+    weather: {
+      [0]: { main },
+    },
+    wind: { speed },
+    wind: { deg },
+  } = await weatherObj;
+  return deg;
 }
 
-getWeather(getData('London'));
+async function runApp() {
+  const result = await getWeather(getData('London'));
+  console.log(result);
+}
+
+runApp();
