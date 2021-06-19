8d34c2aed9f0ba2ab9656498d56c2105d88c66d0
William Addesso <wsaddesso@gmail.com>
2021-06-19 00:12:58 -0400

    Split up getData function into two functions.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
 import './style.css';
 
-// Get weather data and set variables to be used.
+// Make fetch request for city info and return json object.
 async function getData(city) {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=149ffce1bb4cd39b05ac9f60cbf67c14`
     );
-    const cityInfo = await response.json();
-    console.log(cityInfo);
-    const { temp, temp_max, temp_min, humidity } = cityInfo.main;
-    const weatherDescription = cityInfo.weather[0].description;
-    const { deg: windDirection, speed } = cityInfo.wind;
-    return {
-      temp,
-      temp_max,
-      temp_min,
-      humidity,
-      weatherDescription,
-      windDirection,
-      speed,
-    };
+    const responseConverted = await response.json();
+    return responseConverted;
   } catch (error) {
-    console.log(error, 'There was an error getting the weather data');
+    console.log(error, 'There was an error getting the weather info');
   }
 }
 
-// Console logs the returned value from getData which right now is temp
-getData('Eastchester').then((weatherInfo) => console.log(weatherInfo));
+// Return weather info as object.
+async function getWeather(obj) {
+  const data = await obj;
+  console.log(data.main.temp);
+}
+
+getWeather(getData('London'));
